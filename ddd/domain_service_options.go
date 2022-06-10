00b8ce59519cc8b2af56cbb2aa45063addff15e8
liuxd6825 <liuxd6825@163.com>
2022-06-11 00:16:33 +0800

    调整为接口， 可扩展

---
+++ ddd/domain_service_options.go
@@ -0,0 +1,40 @@
+package ddd
+
+type DoCommandOption interface {
+	GetIsValidOnly() *bool
+}
+
+type doCommandOption struct {
+	IsValidOnly *bool
+}
+
+type IsValidOnlyOptions interface {
+	GetIsValidOnly() *bool
+}
+
+func NewDoCommandOption(isValidOnly bool) DoCommandOption {
+	return &doCommandOption{
+		IsValidOnly: &isValidOnly,
+	}
+}
+
+func NewDoCommandOptionMerges(opts ...DoCommandOption) DoCommandOption {
+	res := &doCommandOption{}
+	res.Merges(opts...)
+	return res
+}
+
+func (o *doCommandOption) Merges(opts ...DoCommandOption) {
+	if o == nil {
+		return
+	}
+	for _, opt := range opts {
+		if opt != nil && opt.GetIsValidOnly != nil {
+			o.IsValidOnly = opt.GetIsValidOnly()
+		}
+	}
+}
+
+func (o *doCommandOption) GetIsValidOnly() *bool {
+	return o.IsValidOnly
+}
