d2fac4e184941a69e301db95cbdefa003f77be8b
liuxd6825 <liuxd6825@163.com>
2022-06-15 13:47:30 +0800

    mask复制

--- mapper/mapper.go
+++ mapper/mapper.go
@@ -13,7 +13,7 @@ func init() {
 
 //
 // Mapper
-// @Description: 进行struct属性复制
+// @Description: 进行struct属性复制，支持深度复制
 // @param fromObj 来源
 // @param toObj 目标
 // @return error
@@ -24,7 +24,7 @@ func Mapper(fromObj, toObj interface{}) error {
 
 //
 // MaskMapper
-// @Description: 根据指定进行属性复制
+// @Description: 根据指定进行属性复制，不支持深度复制
 // @param fromObj 来源
 // @param toObj 目标
 // @param mask 要复制属性列表
