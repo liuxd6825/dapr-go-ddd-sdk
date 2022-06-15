23b5b63846cdee6c33b374d4178b6b0b6d2d3b5b
liuxd6825 <liuxd6825@163.com>
2022-06-15 13:48:23 +0800

    mask复制

--- mapper/mapper.go
+++ mapper/mapper.go
@@ -2,6 +2,7 @@ package mapper
 
 import (
 	"github.com/jinzhu/copier"
+	"github.com/mitchellh/mapstructure"
 )
 
 var option *copier.Option
@@ -10,8 +11,57 @@ func init() {
 	option = getOption()
 }
 
+//
+// Mapper
+// @Description: 进行struct属性复制，支持深度复制
+// @param fromObj 来源
+// @param toObj 目标
+// @return error
+//
 func Mapper(fromObj, toObj interface{}) error {
 	return copier.CopyWithOption(toObj, fromObj, *option)
+}
+
+//
+// MaskMapper
+// @Description: 根据指定进行属性复制，不支持深度复制
+// @param fromObj 来源
+// @param toObj 目标
+// @param mask 要复制属性列表
+// @return error
+//
+func MaskMapper(fromObj, toObj interface{}, mask []string) error {
+	var fromMap map[string]interface{}
+	var err error
+	switch fromObj.(type) {
+	case *map[string]interface{}:
+		value := fromObj.(*map[string]interface{})
+		fromMap = *value
+		break
+	case map[string]interface{}:
+		fromMap = fromObj.(map[string]interface{})
+		break
+	default:
+		fromMap = make(map[string]interface{})
+		if err = mapstructure.Decode(fromObj, &fromMap); err != nil {
+			return err
+		}
+	}
+	if len(mask) > 0 {
+		maskMap := make(map[string]string)
+		for _, key := range mask {
+			maskMap[key] = key
+		}
+		for key, _ := range fromMap {
+			_, ok := maskMap[key]
+			if !ok {
+				delete(fromMap, key)
+			}
+		}
+	}
+
+	err = mapstructure.Decode(&fromMap, toObj)
+	return err
 }
 
 func getOption() *copier.Option {
