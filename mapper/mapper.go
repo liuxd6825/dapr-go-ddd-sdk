764bb43caa83742c40bfabadd2e14b276d9efa94
liuxd6825 <liuxd6825@163.com>
2022-06-19 15:44:32 +0800

    解决包嵌套问题

---
+++ mapper/mapper.go
@@ -0,0 +1,26 @@
+package mapper
+
+import "github.com/liuxd6825/dapr-go-ddd-sdk/types"
+
+//
+// Mapper
+// @Description: 进行struct属性复制，支持深度复制
+// @param fromObj 来源
+// @param toObj 目标
+// @return error
+//
+func Mapper(fromObj, toObj interface{}) error {
+	return types.Mapper(fromObj, toObj)
+}
+
+//
+// MaskMapper
+// @Description: 根据指定进行属性复制，不支持深度复制
+// @param fromObj 来源
+// @param toObj 目标
+// @param mask 要复制属性列表
+// @return error
+//
+func MaskMapper(fromObj, toObj interface{}, mask []string) error {
+	return types.MaskMapper(fromObj, toObj, mask)
+}
