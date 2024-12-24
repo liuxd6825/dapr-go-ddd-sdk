10f63925dc2e1b6d3228ab632f7a00b5116afb87
duxm0114 <duxm0114@163.com>
2024-12-24 15:23:13 +0800

    增加json_pkg包，增加NewMap()方法

--- lowcode/hserver/pkg/json_pkg/json.go
+++ lowcode/hserver/pkg/json_pkg/json.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"github.com/liuxd6825/dapr-go-ddd-sdk/errors"
+	"github.com/liuxd6825/dapr-go-ddd-sdk/utils/jsonutils"
 )
 
 type JsonPkg struct {
@@ -33,4 +34,28 @@ func (j *JsonPkg) Format(rawJSON []byte) []byte {
 		panic(errors.New("json format error: %s", err.Error()))
 	}
 	return formattedJSON.Bytes()
+}
+
+// NewMap
+//
+//	@Description:
+//	@receiver j
+//	@param val
+//	@return any
+func (j *JsonPkg) NewMap(jsonVal any) any {
+	var res any = make(map[string]any)
+	var data []byte
+	if str, ok := jsonVal.(string); ok {
+		data = []byte(str)
+	} else if b, ok := jsonVal.([]byte); ok {
+		data = b
+	} else {
+		panic(errors.New("jsonVal is not string"))
+	}
+
+	err := jsonutils.Unmarshal(data, &res)
+	if err != nil {
+		panic(errors.New("json parse error: %s", err.Error()))
+	}
+	return res
 }
--- lowcode/hserver/pkg/json_pkg/json_test.go
+++ lowcode/hserver/pkg/json_pkg/json_test.go
@@ -3,12 +3,17 @@ package json_pkg
 import "testing"
 
 func TestJsonPkg_Format(t *testing.T) {
-	jsonPkg, err := NewJsonPkg()
-	if err != nil {
-		t.Fatal(err)
-	}
+	jsonPkg := NewJsonPkg()
 	// 原始 JSON 字符串
 	rawJSON := `{"name":"John","age":30,"address":{"city":"New York","zip":"10001"},"hobbies":["reading","coding"]}`
 	json := jsonPkg.Format([]byte(rawJSON))
 	t.Log(string(json))
+}
+
+func TestJsonPkg_NewMap(t *testing.T) {
+	jsonPkg := NewJsonPkg()
+	// 原始 JSON 字符串
+	rawJSON := `{"name":"John","age":30,"address":{"city":"New York","zip":"10001"},"hobbies":["reading","coding"]}`
+	json := jsonPkg.NewMap(rawJSON)
+	t.Log(json)
 }
