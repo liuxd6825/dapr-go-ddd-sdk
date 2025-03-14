7c075f8220c8c47191495ad43a8ebc4de665570c
andy <duxm0114@163.com>
2025-03-14 15:21:32 +0800

    rsql 日期格式错误

--- rsql/rsql_sql/process.go
+++ rsql/rsql_sql/process.go
@@ -193,13 +193,22 @@ func (p *Process) getValue(value rsql.Value) any {
 		v = sv.Value
 	case *rsql.DateValue:
 		sv, _ := value.(*rsql.DateValue)
-		v, err = time.Parse(rsql.DateLayout, sv.Value)
+		if date, er := time.Parse(rsql.DateLayout, sv.Value); er != nil {
+			err = er
+		} else {
+			v = fmt.Sprintf("'%v-%v-%v'", date.Year(), date.Month(), date.Day())
+		}
+
 	case *rsql.DoubleValue:
 		sv, _ := value.(*rsql.DoubleValue)
 		v = sv.Value
 	case *rsql.DateTimeValue:
 		sv, _ := value.(*rsql.DateTimeValue)
-		v, err = time.Parse(rsql.DateTimeLayout, sv.Value)
+		if date, er := time.Parse(rsql.DateTimeLayout, sv.Value); er != nil {
+			err = er
+		} else {
+			v = fmt.Sprintf("'%v-%v-%v %v:%v:%v'", date.Year(), date.Month(), date.Day(), date.Hour(), date.Minute(), date.Second())
+		}
 	case *rsql.BooleanValue:
 		sv, _ := value.(*rsql.BooleanValue)
 		v = sv.Value
