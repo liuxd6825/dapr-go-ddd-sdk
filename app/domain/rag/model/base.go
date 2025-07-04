7023ce63df20655dde69d80aaa182e0ec873bd35
andy <duxm0114@163.com>
2025-07-04 14:03:00 +0800

    dao升级

--- app/domain/rag/model/base.go
+++ app/domain/rag/model/base.go
@@ -3,10 +3,10 @@ package model
 import "time"
 
 type Base struct {
-	Id          string     `gorm:"id;primaryKey" json:"id" bson:"id"`
-	TenantId    string     `gorm:"tenant_id" json:"tenantId,omitempty" bson:"tenant_id"` // 租户ID
-	CaseId      string     `gorm:"case_id" json:"caseId,omitempty" bson:"case_id"`
-	Remark      string     `gorm:"remark" json:"remark,omitempty" bson:"remark"`
+	Id          string     `gorm:"id;primaryKey;title:主键" json:"id" bson:"id"`
+	TenantId    string     `gorm:"tenant_id;title:租户ID" json:"tenantId,omitempty" bson:"tenant_id"` // 租户ID
+	CaseId      string     `gorm:"case_id;title:案件ID" json:"caseId,omitempty" bson:"case_id"`
+	Remark      string     `gorm:"remark;title:备注" json:"remark,omitempty" bson:"remark"`
 	CreatedTime *time.Time `gorm:"created_time" json:"createdTime,omitempty" bson:"created_time"`
 	CreatorId   string     `gorm:"creator_id" json:"creatorId,omitempty" bson:"creator_id"`
 	CreatorName string     `gorm:"creator_name" json:"creatorName,omitempty" bson:"creator_name"`
