940d280551e5e7033ddd71af8ba202a6f2712495
andy <duxm0114@163.com>
2025-07-08 20:46:28 +0800

    流水导入与数据库mcp服务

---
+++ app/domain/master/cmdquery/record_query.go
@@ -0,0 +1,54 @@
+package cmdquery
+
+import (
+	"fmt"
+	"github.com/liuxd6825/dapr-go-ddd-sdk/ddd/ddd_query"
+	"github.com/liuxd6825/dapr-go-ddd-sdk/pkg/errors"
+)
+
+// RecordFindByIdQuery 按ID查询命令
+type RecordFindByIdQuery = ddd_query.FindByIdQuery
+
+// RecordFindByIdsQuery 按多个ID查询命令
+type RecordFindByIdsQuery = ddd_query.FindByIdsQuery
+
+// RecordFindAllQuery 查询所有命令
+type RecordFindAllQuery struct {
+	TenantId string `json:"tenantId"`
+}
+
+// RecordFindByCaseIdQuery 按聚合根ID查询命令
+type RecordFindByCaseIdQuery struct {
+	ddd_query.FindPagingQuery
+	CaseId string `json:"caseId"`
+}
+
+// RecordFindByDocIdQuery 按聚合根ID查询命令
+type RecordFindByDocIdQuery struct {
+	TenantId string `json:"tenantId"`
+	CaseId   string `json:"caseId"`
+	DocId    string `json:"docId"`
+}
+
+type RecordFindByTaskIdQuery struct {
+	TenantId string `json:"tenantId"`
+	CaseId   string `json:"caseId"`
+	TaskId   string `json:"taskId"`
+}
+
+// RecordFindByFileIdQuery 按聚合根ID查询命令
+type RecordFindByFileIdQuery struct {
+	TenantId string `json:"tenantId"`
+	CaseId   string `json:"caseId"`
+	FileId   string `json:"fileId"`
+}
+
+type RecordFindPagingQuery = ddd_query.FindPagingQuery
+type RecordFindPagingResult = ddd_query.FindPagingResult
+
+func (q *RecordFindByCaseIdQuery) GetMustWhere() (string, error) {
+	if len(q.CaseId) == 0 {
+		return "", errors.New("caseId参数不能为空")
+	}
+	return fmt.Sprintf("CaseId=='%s'", q.CaseId), nil
+}
