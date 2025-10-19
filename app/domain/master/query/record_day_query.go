15d699d57186b2cc29a1e1c4d8d519b80550ad00
andy <duxm0114@163.com>
2025-10-19 16:11:23 +0800

    增加流水日汇总api

--- app/domain/master/query/record_day_query.go
+++ app/domain/master/query/record_day_query.go
@@ -5,23 +5,20 @@ import "github.com/liuxd6825/dapr-go-ddd-sdk/ddd/ddd_query"
 // RecordDayFindByIdQuery 按ID查询命令
 type RecordDayFindByIdQuery = ddd_query.FindByIdQuery
 
+// RecordDayFindByCaseIdQuery 按聚合根ID查询命令
+type RecordDayFindByCaseIdQuery = ddd_query.FindPagingByCaseIdQuery
+
 type RecordDayFindBySumChartQuery struct {
-	CaseId      string
-	SummaryType string
-	Filter      string
+	CaseId      string `json:"caseId"  param:"caseId" validate:"required" title:"项目ID"`
+	SummaryType string `json:"summaryType"  param:"summaryType" validate:"required" title:"汇总方式"`
+	Filter      string `json:"filter"  param:"filter" validate:"required" title:"过滤条件"`
 }
 
 type RecordDayFindBySumTableQuery struct {
-	CaseId      string
-	Filter      string
-	GroupFilter string
-	Sort        string
-	PageSize    int64
-	PageNum     int64
-}
-
-// RecordDayFindByCaseIdQuery 按聚合根ID查询命令
-type RecordDayFindByCaseIdQuery struct {
-	ddd_query.FindPagingQuery
-	CaseId string `json:"caseId" query:"case-id"`
+	CaseId      string `json:"caseId"  param:"caseId" validate:"required" title:"项目ID"`
+	Filter      string `json:"filter"  param:"filter" validate:"required" title:"过滤条件"`
+	GroupFilter string `json:"groupFilter"  param:"groupFilter"  title:"分组条件"`
+	Sort        string `json:"sort"  param:"sort" title:"排序"`
+	PageSize    int64  `json:"pageSize"  param:"pageSize" title:"分页大小"`
+	PageNum     int64  `json:"pageNum"  param:"pageNum"  title:"分页页号"`
 }
