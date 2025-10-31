0374799a609fdd067c00343817d7c45d21a19823
ccu_lw <ccu_lw@163.com>
2025-10-31 20:02:59 +0800

    bug

--- app/domain/sys/portal/model/tenant.go
+++ app/domain/sys/portal/model/tenant.go
@@ -9,14 +9,13 @@ type Tenant struct {
 	Base       `bson:",inline"`
 	Name       string            `json:"name" gorm:"name" bson:"name"`
 	Code       string            `json:"code" gorm:"code" bson:"code"`
-	Account    string            `json:"account" gorm:"account" bson:"account"`
 	Address    string            `json:"address" gorm:"address" bson:"address"`
 	Phone      string            `json:"phone" gorm:"phone" bson:"phone"`
 	Email      string            `json:"email" gorm:"email" bson:"email"`
-	EffectDate *time.Time        `json:"effect_date" gorm:"effect_date" bson:"effect_date"` //生效日期
-	ExpireDate *time.Time        `json:"expire_date" gorm:"expire_date" bson:"expire_date"` //过期日期
-	LogoutDate *time.Time        `json:"logout_date" gorm:"logout_date" bson:"logout_date"` //注销日期
-	Status     enum.TenantStatus `json:"status" gorm:"status" bson:"status"`                // 状态
+	EffectDate *time.Time        `json:"effectDate" gorm:"effect_date" bson:"effect_date"` //生效日期
+	ExpireDate *time.Time        `json:"expireDate" gorm:"expire_date" bson:"expire_date"` //过期日期
+	LogoutDate *time.Time        `json:"logoutDate" gorm:"logout_date" bson:"logout_date"` //注销日期
+	Status     enum.TenantStatus `json:"status" gorm:"status" bson:"status"`               // 状态
 }
 
 func NewTenant() (*Tenant, error) {
