6f1069b54bac3995d313aca16a2d19503fcfb772
andy <duxm0114@163.com>
2025-11-26 19:44:41 +0800

    重命名

--- app/xcommon/xbase/event_test.go
+++ app/xcommon/xbase/event_test.go
@@ -1,33 +1,39 @@
 package xbase
 
 import (
+	"context"
 	"testing"
 
+	"github.com/liuxd6825/dapr-go-ddd-sdk/pkg/events"
 	xtest2 "github.com/liuxd6825/dapr-go-ddd-sdk/pkg/xtest"
 	"github.com/stretchr/testify/assert"
 )
 
-type RecordImportMasterEvent struct {
-	Command[*TestCommandData]
-}
-type TestCommandData struct {
+type UnitTestEvent = events.Event[*UnitTestData]
+type UnitTestData struct {
 	Id   string `json:"id"`
 	Name string `json:"name"`
 	Age  int    `json:"age"`
 }
 
+const UnitTestEventType = "test.unit.test-event"
+
+func NewUnitTestEvent(ctx context.Context, data *UnitTestData) *UnitTestEvent {
+	event := &UnitTestEvent{}
+	event.SetData(ctx, "", data, &events.EventOptions{EventType: UnitTestEventType})
+	return event
+}
 func Test_PublishEvent(t *testing.T) {
 	xtest2.InitEnv_MongoLocal(xtest2.NewMongoOptions().SetDBName("master"))
 	ctx := xtest2.NewContext()
-	cmd := &RecordImportMasterEvent{}
-	cmd.SetData(&TestCommandData{
+	event := NewUnitTestEvent(ctx, &UnitTestData{
 		Id:   "1",
 		Name: "张三",
 		Age:  18,
 	})
-	meta := map[string]any{
+	event.Meta = map[string]any{
 		"key": "value",
 	}
-	err := PublishEvent(ctx, "test-app", cmd, meta)
+	err := PublishEvent(ctx, event)
 	assert.Nil(t, err)
 }
