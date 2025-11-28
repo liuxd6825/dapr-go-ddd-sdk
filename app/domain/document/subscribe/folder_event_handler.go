8ae4e6c53746a50b331f2cbb35dc9444880d30fd
andy <duxm0114@163.com>
2025-11-28 18:16:00 +0800

    1：app/xcommon移动到app/pkg/xcommon 2：增加scriggo包实现lowcode 3：修改流水导入规则

--- app/domain/document/subscribe/folder_event_handler.go
+++ app/domain/document/subscribe/folder_event_handler.go
@@ -2,14 +2,15 @@ package subscribe
 
 import (
 	"context"
+	"time"
+
 	model2 "github.com/liuxd6825/dapr-go-ddd-sdk/app/domain/document/model"
 	"github.com/liuxd6825/dapr-go-ddd-sdk/app/domain/document/service"
 	"github.com/liuxd6825/dapr-go-ddd-sdk/pkg/utils/idutils"
-	"time"
 
 	"github.com/kataras/iris/v12"
 	"github.com/liuxd6825/dapr-go-ddd-sdk/app/domain/document/event"
-	"github.com/liuxd6825/dapr-go-ddd-sdk/app/xcommon/config"
+	"github.com/liuxd6825/dapr-go-ddd-sdk/app/pkg/xcommon/config"
 	"github.com/liuxd6825/dapr-go-ddd-sdk/pkg/db/dao/store"
 	"github.com/liuxd6825/dapr-go-ddd-sdk/pkg/db/tx"
 	"github.com/liuxd6825/dapr-go-ddd-sdk/pkg/env"
